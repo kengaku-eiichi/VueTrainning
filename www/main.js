2b3735ecf6f8cfee1c8bb67fda6d199f0e34015f
kengaku <keng9358@gmail.com>
2020-05-21 01:06:03 +0900

    list8

--- www/main.js
+++ www/main.js
@@ -1,10 +1,6 @@
 var app = new Vue({
     el: '#app',
     data: {
-        products: [
-            { code: 'A01', name: 'プロダクトA' },
-            { code: 'B01', name: 'プロダクトB' },
-            { code: 'C01', name: 'プロダクトC' }
-        ]
+        price: 980
     }
 })
