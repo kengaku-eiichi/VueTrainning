d87b40d624f208c0f7c5939b945f34024d04022f
kengaku <keng9358@gmail.com>
2020-05-21 11:26:19 +0900

    P.87 list3

--- www/main.js
+++ www/main.js
@@ -10,8 +10,13 @@ var app = new Vue({
         }
     },
     computed: {
-        statusMessage: function (newStock, oldStock) {
+        statusMessage: function () {
             return this.stock == 0 ? '売り切れ' : ''
+        }
+    },
+    watch: {
+        statusMessage: function (newValue, oldValue) {
+            console.log('商品のステータスメッセージが「' + oldValue + '」から「' + newValue + '」に変更されました')
         }
     }
 })
