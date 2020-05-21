716a0768e9ad77a134d765f33a4c8a44148eb0ad
kengaku <keng9358@gmail.com>
2020-05-21 10:22:00 +0900

    P.73 list1

--- www/main.js
+++ www/main.js
@@ -1,14 +1,15 @@
 var app = new Vue({
     el: '#app',
     data: {
-        price: 1000
+        year: (new Date).getFullYear()
     },
-    filters: {
-        number_format: function (val) {
-            return val.toLocaleString()
-        },
-        unit: function (val) {
-            return val + '円';
+    computed: {
+        isUrudoshi: function () {
+            if ((this.year % 4 == 0 && this.year % 100 != 0) || this.year % 400 == 0) {
+                return true;
+            } else {
+                return false
+            }
         }
     }
 })
