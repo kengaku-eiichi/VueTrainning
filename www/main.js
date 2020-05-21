85f3bd4e1570572c417e715aaec6dacf07cf3872
kengaku <keng9358@gmail.com>
2020-05-21 09:56:15 +0900

    P67-69 list1,2

--- www/main.js
+++ www/main.js
@@ -1,6 +1,10 @@
+Vue.filter('number_format', function (val) {
+    return val.toLocaleString();
+});
+
 var app = new Vue({
     el: '#app',
     data: {
-        price: 1280
+        price: 1000
     }
 })
