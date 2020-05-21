76efda4569150809ef719aa2eb335ad8f45b031c
kengaku <keng9358@gmail.com>
2020-05-21 12:29:24 +0900

    P.90 list2

--- www/main.js
+++ www/main.js
@@ -2,5 +2,10 @@ var app = new Vue({
     el: '#app',
     data: {
         year: (new Date).getFullYear()
+    },
+    methods: {
+        yearInputHandler: function (e) {
+            this.year = e.target.value;
+        }
     }
 })
