3d2b6f87791a310263042b5f436a0b0fea600367
kengaku <keng9358@gmail.com>
2020-05-21 12:24:22 +0900

    P.88 list1

--- www/main.js
+++ www/main.js
@@ -1,22 +1,6 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: '',
-        stock: 10
-    },
-    methods: {
-        onDeleteItem: function () {
-            this.stock--
-        }
-    },
-    computed: {
-        statusMessage: function () {
-            return this.stock == 0 ? '売り切れ' : ''
-        }
-    },
-    watch: {
-        statusMessage: function (newValue, oldValue) {
-            console.log('商品のステータスメッセージが「' + oldValue + '」から「' + newValue + '」に変更されました')
-        }
+        year: (new Date).getFullYear()
     }
 })
