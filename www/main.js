eb6c246d920a449ce386c14eb7e20a16a510cc0b
kengaku <keng9358@gmail.com>
2020-05-21 15:30:03 +0900

    P.104 list12

--- www/main.js
+++ www/main.js
@@ -1,21 +1,27 @@
 var app = new Vue({
     el: '#app',
     data: {
-        arrival_date: null,
-        min_date: null
+        color: '#000000',
+        red: 0,
+        green: 0,
+        blue: 0
     },
-    created: function () {
-        var dt = new Date;
-        dt.setDate(dt.getDate() + 1);
-        this.arrival_date = this.formatDate(dt);
-        this.min_date = this.arrival_date;
+    computed: {
+        colorElement: function () {
+            return [this.red, this.green, this.blue];
+        }
     },
-    methods: {
-        formatDate: function (dt) {
-            var y = dt.getFullYear();
-            var m = ('00' + (dt.getMonth() + 1)).slice(-2);
-            var d = ('00' + dt.getDate()).slice(-2);
-            return y + '-' + m + '-' + d;
+    watch: {
+        colorElement: function (newValue, oldValue) {
+            var r = ('00' + newValue[0].toString(16).toUpperCase()).slice(-2);
+            var g = ('00' + newValue[1].toString(16).toUpperCase()).slice(-2);
+            var b = ('00' + newValue[2].toString(16).toUpperCase()).slice(-2);
+            this.color = '#' + r + g + b;
+        },
+        color: function (newValue, oldValue) {
+            this.red = parseInt(newValue.substr(1, 2), 16);
+            this.green = parseInt(newValue.substr(3, 2), 16);
+            this.blue = parseInt(newValue.substr(5, 2), 16);
         }
     }
 })
