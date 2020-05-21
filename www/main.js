b5f520a3dc6c7808f6c2617f4afacd6c8045291c
kengaku <keng9358@gmail.com>
2020-05-21 10:09:46 +0900

    list4

--- www/main.js
+++ www/main.js
@@ -1,10 +1,14 @@
-Vue.filter('number_format', function (val) {
-    return val.toLocaleString();
-});
-
 var app = new Vue({
     el: '#app',
     data: {
         price: 1000
+    },
+    filters: {
+        number_format: function (val) {
+            return val.toLocaleString()
+        },
+        unit: function (val) {
+            return val + '円';
+        }
     }
 })
