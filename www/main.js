0e90afa03ccbd6b425211bb2939ecf28ac665781
kengaku <keng9358@gmail.com>
2020-05-22 22:30:32 +0900

    P.140-141

--- www/main.js
+++ www/main.js
@@ -1,71 +1,17 @@
-var nodeApp = document.querySelector('#app');
-
-var nodeCheckbox = nodeApp.querySelectorAll('input[type="checkbox"]');
-nodeCheckbox[0].addEventListener('change', onCheckChanged, false);
-nodeCheckbox[1].addEventListener('change', onCheckChanged, false);
-
-var nodeSelect = nodeApp.querySelector('.sorting');
-nodeSelect.addEventListener('change', onOrderChanged, false);
-
-var nodeItemsOrg = nodeApp.querySelectorAll('.item');
-
-function onCheckChanged(e) {
-    var nodeItems = nodeApp.querySelectorAll('.item');
-    var nodeCount = nodeApp.querySelector('.count');
-
-    var showExpression = nodeCheckbox[0].checked && nodeCheckbox[1].checked ? function (nodeItem) {
-        return isSaleItem(nodeItem) && isDelvFreeItem(nodeItem);
+var app = new Vue({
+    el: '#app',
+    data: {
+        count: 0,
+        showSaleItem: false,
+        showDelvFree: false,
+        sortOrder: 1,
+        products: [
+            { name: 'Michael<br>スマホケース', price: 1580, image: 'images/01.jpg', delv: 0, isSale: true },
+            { name: 'Raphael<br>スマホケース', price: 1580, image: 'images/02.jpg', delv: 0, isSale: true },
+            { name: 'Gabriel<br>スマホケース', price: 1580, image: 'images/03.jpg', delv: 240, isSale: true },
+            { name: 'Uriel<br>スマホケース', price: 980, image: 'images/04.jpg', delv: 0, isSale: true },
+            { name: 'Ariel<br>スマホケース', price: 980, image: 'images/05.jpg', delv: 0, isSale: false },
+            { name: 'Azrael<br>スマホケース', price: 1580, image: 'images/06.jpg', delv: 0, isSale: false }
+        ]
     }
-        : nodeCheckbox[0].checked ? isSaleItem
-            : nodeCheckbox[1].checked ? isDelvFreeItem
-                : function (nodeItem) { return true; };
-
-    var count = 0;
-    for (var i = 0; i < nodeItems.length; i++) {
-        if (showExpression(nodeItems[i])) {
-            showNode(nodeItems[i]);
-            count++;
-        } else {
-            hideNode(nodeItems[i]);
-        }
-    }
-
-    nodeCount.textContent = count + '件';
-}
-
-function onOrderChanged(e) {
-    var nodeList = nodeApp.querySelector('.list');
-
-    while (nodeList.firstChild) nodeList.firstChild.remove();
-
-    if (e.target.value == '1') {
-        for (var i = 0; i < nodeItemsOrg.length; i++) nodeList.appendChild(nodeItemsOrg[i]);
-    } else if (e.target.value == '2') {
-        var products = [];
-        for (var i = 0; i < nodeItemsOrg.length; i++) products.push(nodeItemsOrg[i]);
-        products.sort(function (a, b) {
-            var priceA = parseInt(a.querySelector('.price span').textContent.replace(',', ''));
-            var priceB = parseInt(b.querySelector('.price span').textContent.replace(',', ''));
-            return priceA - priceB;
-        });
-        for (var i = 0; i < products.length; i++) nodeList.appendChild(products[i]);
-    }
-}
-
-function isSaleItem(nodeItem) {
-    var node = nodeItem.querySelector('.status');
-    return node && node.textContent == 'SALE';
-}
-
-function isDelvFreeItem(nodeItem) {
-    var node = nodeItem.querySelector('.shipping-fee');
-    return node && node.textContent == '送料無料';
-}
-
-function hideNode(node) {
-    node.setAttribute('style', 'display:none;');
-}
-
-function showNode(node) {
-    node.removeAttribute('style');
-}
+});
