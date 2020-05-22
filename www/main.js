0f4af38d471133f572058d51f7d4db2ef1d3ebc7
kengaku <keng9358@gmail.com>
2020-05-22 23:06:12 +0900

    検索実装

--- www/main.js
+++ www/main.js
@@ -15,6 +15,15 @@ var app = new Vue({
             { name: 'Uriel<br>スマホケース', price: 980, image: 'images/04.jpg', delv: 0, isSale: true },
             { name: 'Ariel<br>スマホケース', price: 980, image: 'images/05.jpg', delv: 0, isSale: false },
             { name: 'Azrael<br>スマホケース', price: 1580, image: 'images/06.jpg', delv: 0, isSale: false }
-        ]
+        ],
+        showItem: true
+    },
+    methods: {
+        showExpression: function (product) {
+            return this.showSaleItem && this.showDelvFree ? product.isSale && product.delv == 0
+                : this.showSaleItem ? product.isSale
+                    : this.showDelvFree ? product.delv == 0
+                        : true;
+        }
     }
 });
