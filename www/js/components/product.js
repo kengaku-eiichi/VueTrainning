0a0ef316b29bf7314c4453f9b0660cb4f5d45410
kengaku <keng9358@gmail.com>
2020-05-25 00:13:53 +0900

    * componentにイベントハンドラを実装 * product を bind するよう、修正

--- www/js/components/product.js
+++ www/js/components/product.js
@@ -1,7 +1,13 @@
 Vue.component('product', {
     template: `
         <div>
-            <span>{{name}}</span>：<span>{{price}}（円）</span>
+            <span>{{product.name}}</span>：<span>{{product.price}}（円）</span>
+            <button v-on:click="onPriceDownClick">値下げする</button>
         </div>`,
-    props: ['name', 'price']
+    props: ['product'],
+    methods: {
+        onPriceDownClick: function (e) {
+            this.$emit('price-down-click', this);
+        }
+    }
 });
