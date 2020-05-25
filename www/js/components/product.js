0db0a11eca0c78e5e3186b61e26162189d8e4c07
kengaku <keng9358@gmail.com>
2020-05-25 10:29:47 +0900

    component で tr を使用

--- www/js/components/product.js
+++ www/js/components/product.js
@@ -1,9 +1,10 @@
 Vue.component('product', {
     template: `
-        <div>
-            <span>{{product.name}}</span>：<span>{{product.price}}（円）</span>
-            <button v-on:click="onPriceDownClick">値下げする</button>
-        </div>`,
+        <tr>
+            <td>{{product.name}}</td>
+            <td style="text-align:right;">{{product.price}}（円）</td>
+            <td><button v-on:click="onPriceDownClick">値下げする</button></td>
+        </tr>`,
     props: ['product'],
     methods: {
         onPriceDownClick: function (e) {
