c0de435ef050310026089d2a6ce9159d5227fc31
kengaku <keng9358@gmail.com>
2020-05-27 23:12:31 +0900

    cp コマンドに -f オプションを追加し、上書きを強制

--- centos7/nginx.sh
+++ centos7/nginx.sh
@@ -5,8 +5,8 @@ yum info installed nginx || {
     yum -y install nginx
 
     mkdir -p /vagrant/etc/nginx/conf.d.org
-    cp /etc/nginx/nginx.conf /vagrant/etc/nginx/nginx.conf.org
-    cp -n /etc/nginx/conf.d/* /vagrant/etc/nginx/conf.d.org/
+    \cp -f /etc/nginx/nginx.conf /vagrant/etc/nginx/nginx.conf.org
+    \cp -f /etc/nginx/conf.d/* /vagrant/etc/nginx/conf.d.org/
 }
 \cp -f /vagrant/etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf
 systemctl restart nginx.service
