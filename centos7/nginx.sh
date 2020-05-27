8eefb388c2cbd72fbe8e36529ff4d0a62a54dc92
kengaku <keng9358@gmail.com>
2020-05-27 23:07:45 +0900

    nginx設定ファイルのデフォルトを退避 nginx設定ファイルのデフォルトを.gitignore に追加
    @vue/cli の取得先を yarn に変更

--- centos7/nginx.sh
+++ centos7/nginx.sh
@@ -4,9 +4,9 @@ yum info installed nginx || {
     rpm -ivh http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm
     yum -y install nginx
 
-    mkdir -p /vagrant/etc/nginx
+    mkdir -p /vagrant/etc/nginx/conf.d.org
     cp /etc/nginx/nginx.conf /vagrant/etc/nginx/nginx.conf.org
-    cp -pr /etc/nginx/conf.d /vagrant/etc/nginx/
+    cp -n /etc/nginx/conf.d/* /vagrant/etc/nginx/conf.d.org/
 }
 \cp -f /vagrant/etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf
 systemctl restart nginx.service
--- centos7/node.sh
+++ centos7/node.sh
@@ -4,18 +4,27 @@ yum info installed git || {
     yum -y install git
 }
 
+cd /git/VueTrainning/
 nvm --version || {
     git clone git://github.com/creationix/nvm.git /opt/nvm
     ln -snf /opt/nvm/nvm.sh /etc/profile.d/.
-    source /opt/nvm/nvm.sh
+    source /etc/profile.d/nvm.sh
     nvm --version
 
+    # nvm install --lts
+    # nvm use --lts
     nvm install stable
     nvm use stable
-    node --version
+
+    node -v
     npm -v
 
-    npm install -g @vue/cli
-    vue --version
+    npm i -g yarn
+    yarn -v
+
+    yarn global add @vue/cli
+    vue -V
 }
 nohup vue ui --host 0.0.0.0 --port 8000 --headless --quiet >/dev/null 2>&1 &
+
+
