7e18d79fc6b99d5880510709de408c459b57ee64
kengaku <keng9358@gmail.com>
2020-05-28 12:27:31 +0900

    商品一覧をvue projectで実装

---
+++ product-list/src/components/product-search.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="search">
+    <div class="result">
+      <div v-if="result.isError" class="error">{{result.message}}</div>
+      <template v-else>
+        検索結果
+        <span class="count">{{result.list.length}}件</span>
+      </template>
+    </div>
+    <div class="condition">
+      <div class="target">
+        <label>
+          <input type="checkbox" v-model="condition.showSaleItem" /> セール対象
+        </label>
+        <label>
+          <input type="checkbox" v-model="condition.showDelvFree" /> 送料無料
+        </label>
+      </div>
+      <div class="sort">
+        <label for="sort">並び替える</label>
+        <select id="sort" class="sorting" v-model.number="condition.sortOrder">
+          <option value="1">標準</option>
+          <option value="2">価格が安い順</option>
+        </select>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["condition", "result"]
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.search {
+  display: flex;
+  justify-content: space-between;
+}
+.search .target {
+  display: inline-block;
+}
+.search .target label {
+  margin-right: 15px;
+}
+.search .sort {
+  display: inline-block;
+}
+</style>
--- product-list/src/main.js
+++ product-list/src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import jQuery from 'jquery'
+global.jquery = jQuery
+global.$ = jQuery
+window.$ = window.jQuery = require('jquery')
 
 Vue.config.productionTip = false
 
